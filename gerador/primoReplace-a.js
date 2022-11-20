517f24f7c947a2250ecdde67aa6396124a0fd5a6
herbert366 <herbert.artur18@gmail.com>
2022-11-19 21:00:29 -0300

    finished script

--- gerador/primoReplace-a.js
+++ gerador/primoReplace-a.js
@@ -3,18 +3,73 @@ export const replace = `
 já que ninguém comeu
 {já que} {ninguém fez isso}
 
+já que ninguém estudava
+{já que} {ninguém fez isso}
+
+desde que ninguém estudava
+{já que} {ninguém fez isso}
+
+
 {since nobody did it}
 {since {nobody did it}
 
 ficará isolado
 {vai ficar isolado}
 
+{vai ficar isolado}
+{vai} {ficar isolado}
+
+{will be isolated}
+{will} {be isolated}
+
 porque não puderam
 {porque eles não podiam}
+
+porque não podiam
+{porque} {eles não podiam}
+
+{porque eles não podiam}
+{porque} {eles não podiam}
+
+{because they couldn't}
+{because} {they couldn't}
 
 {usado para estudar}
 {costumava estudar}
 
+{vai comer salada}
+{vai} comer salada}
 
+{will eat salad}
+{will} {eat salad}
 
+{está mostrando suas ideias}
+{está mostrando} {suas ideias}
+
+{is showing your ideas}
+{is showing} {your ideas}
+
+{comprou novos discos}
+{comprou} {novos discos}
+
+{bought new records}
+{bought} {new records}
+
+{vai proibir fantasias}
+{vai} {proibir fantasias}
+
+{will ban costumes}
+{will} {ban costumes}
+
+{vai comer saudável}
+{vai} {comer saudável}
+
+{will eat healthy}
+{will} {eat healthy}
+
+essa criança
+{aquela criança}
+
+vai ficar isolada
+{vai} {ficar isolada}
 `
--- script/primo/doing-a.js
+++ script/primo/doing-a.js
@@ -2,40 +2,40 @@ export const rawScript = `
 {já que} {ninguém fez isso}
 {since} {nobody did it}
 
-{vai comer salada}
-{will eat salad}
+{vai} {comer salada}
+{will} {eat salad}
 
-{zoe} {vai comer salada} {já que} {ninguém fez isso}
-{zoe} {will eat salad} {since} {nobody did it}       
+{zoe} {vai} {comer salada} {já que} {ninguém fez isso}
+{zoe} {will} {eat salad} {since} {nobody did it}       
 
-{joe} {vai comer salada} {já que} {ninguém fez isso}
-{joe} {will eat salad} {since} {nobody did it}       
+{joe} {vai} {comer salada} {já que} {ninguém fez isso}
+{joe} {will} {eat salad} {since} {nobody did it}       
 
-{está mostrando suas ideias}
-{is showing your ideas}
+{está mostrando} {suas ideias}
+{is showing} {your ideas}
 
-{joe} {está mostrando suas ideias} {já que} {ninguém fez isso}
-{joe} {is showing your ideas} {since} {nobody did it}
+{joe} {está mostrando} {suas ideias} {já que} {ninguém fez isso}
+{joe} {is showing} {your ideas} {since} {nobody did it}
 
-{vai ficar isolado}
-{will be isolated}
+{vai} {ficar isolado}
+{will} {be isolated}
 
-{porque eles não podiam}
-{because they couldn't}
+{porque} {eles não podiam}
+{because} {they couldn't}
 
-{joe} {vai ficar isolado} {porque eles não podiam}
-{joe} {will be isolated} {because they couldn't}   
+{joe} {vai} {ficar isolado} {porque} {eles não podiam}
+{joe} {will} {be isolated} {because} {they couldn't}   
 
-{comprou novos discos}
-{bought new records}
+{comprou} {novos discos}
+{bought} {new records}
 
-{zoe} {comprou novos discos} {porque eles não podiam}
-{zoe} {bought new records} {because they couldn't} 
+{zoe} {comprou} {novos discos} {porque} {eles não podiam}
+{zoe} {bought} {new records} {because} {they couldn't} 
 
 {costumava estudar}
 {used to study}
 
-{joe} costumava estudar já que ninguém estudava
+{joe} costumava estudar {já que} {ninguém fez isso}
 {joe} {used to study} {since} {nobody did it}
 
 {meu chefe}
@@ -44,92 +44,92 @@ export const rawScript = `
 {estudou finanças}
 {studied finance}
 
-{meu chefe} {estudou finanças} {porque eles não podiam}
-{my boss} {studied finance} {because they couldn't}
+{meu chefe} {estudou finanças} {porque} {eles não podiam}
+{my boss} {studied finance} {because} {they couldn't}
 
-{vai proibir fantasias}
-{will ban costumes}
+{vai} {proibir fantasias}
+{will} {ban costumes}
 
-{joe} {vai proibir fantasias} {porque eles não podiam}
-{joe} {will ban costumes} {because they couldn't}
+{joe} {vai} {proibir fantasias} {porque} {eles não podiam}
+{joe} {will} {ban costumes} {because} {they couldn't}
 
-{zoe} estudava desde que ninguém estudava
+{zoe} costumava estudar {já que} {ninguém fez isso}
 {zoe} {used to study} {since} {nobody did it}
 
-{zoe} {vai proibir fantasias} porque não podiam
-{zoe} {will ban costumes} {because they couldn't}
+{zoe} {vai} {proibir fantasias} {porque} {eles não podiam}
+{zoe} {will} {ban costumes} {because} {they couldn't}
 
-{zoe} costumava estudar {porque eles não podiam}
-{zoe} {used to study} {because they couldn't}
+{zoe} costumava estudar {porque} {eles não podiam}
+{zoe} {used to study} {because} {they couldn't}
 
 {meu chefe} {estudou finanças} {já que} {ninguém fez isso}
 {my boss} {studied finance} {since} {nobody did it}
 
-{zoe} {comprou novos discos} {já que} {ninguém fez isso}
-{zoe} {bought new records} {since} {nobody did it}
+{zoe} {comprou} {novos discos} {já que} {ninguém fez isso}
+{zoe} {bought} {new records} {since} {nobody did it}
 
-{Joe} {comprou novos discos} {porque eles não podiam}
-{joe} {bought new records} {because they couldn't}
+{Joe} {comprou} {novos discos} {porque} {eles não podiam}
+{joe} {bought} {new records} {because} {they couldn't}
 
-{vai comer saudável}
-{will eat healthy}
+{vai} {comer saudável}
+{will} {eat healthy}
 
-{meu chefe} {vai comer saudável} {já que} {ninguém fez isso}
-{my boss} {will eat healthy} {since} {nobody did it}
+{meu chefe} {vai} {comer saudável} {já que} {ninguém fez isso}
+{my boss} {will} {eat healthy} {since} {nobody did it}
 
 {aquela criança}
 {that child}
 
-essa criança {vai comer salada} {já que} {ninguém fez isso}
-{that child} {will eat salad} {since} {nobody did it}
+{aquela criança} {vai} {comer salada} {já que} {ninguém fez isso}
+{that child} {will} {eat salad} {since} {nobody did it}
 
-{zoe} {vai comer saudável} {já que} {ninguém fez isso}
-{zoe} {will eat healthy} {since} {nobody did it}
+{zoe} {vai} {comer saudável} {já que} {ninguém fez isso}
+{zoe} {will} {eat healthy} {since} {nobody did it}
 
-{zoe} {vai comer salada} {porque eles não podiam}
-{zoe} {will eat salad} {because they couldn't}
+{zoe} {vai} {comer salada} {porque} {eles não podiam}
+{zoe} {will} {eat salad} {because} {they couldn't}
 
-{joe} {vai comer salada} {porque eles não podiam}
-{joe} {will eat salad} {because they couldn't}
+{joe} {vai} {comer salada} {porque} {eles não podiam}
+{joe} {will} {eat salad} {because} {they couldn't}
 
-{zoe} vai ficar isolada {porque eles não podiam}
-{zoe} {will be isolated} {because they couldn't}
+{zoe} {vai} {ficar isolada} {porque} {eles não podiam}
+{zoe} {will} {be isolated} {because} {they couldn't}
 
-{joe} {vai ficar isolado} {já que} {ninguém fez isso}
-{joe} {will be isolated} {since} {nobody did it}
+{joe} {vai} {ficar isolado} {já que} {ninguém fez isso}
+{joe} {will} {be isolated} {since} {nobody did it}
 
-{joe} costumava estudar {porque eles não podiam}
-{joe} {used to study} {because they couldn't}
+{joe} costumava estudar {porque} {eles não podiam}
+{joe} {used to study} {because} {they couldn't}
 
-{joe} {comprou novos discos} {já que} {ninguém fez isso}
-{joe} {bought new records} {since} {nobody did it}
+{joe} {comprou} {novos discos} {já que} {ninguém fez isso}
+{joe} {bought} {new records} {since} {nobody did it}
 
-{zoe} {está mostrando suas ideias} {já que} {ninguém fez isso}
-{zoe} {is showing your ideas} {since} {nobody did it}
+{zoe} {está mostrando} {suas ideias} {já que} {ninguém fez isso}
+{zoe} {is showing} {your ideas} {since} {nobody did it}
 
-{joe} {vai comer saudável} {já que} {ninguém fez isso}
-{joe} {will eat healthy} {since} {nobody did it}
+{joe} {vai} {comer saudável} {já que} {ninguém fez isso}
+{joe} {will} {eat healthy} {since} {nobody did it}
 
-{meu chefe} {vai proibir fantasias} já que ninguém fez
-{my boss} {will ban costumes} {since} {nobody did it}
+{meu chefe} {vai} {proibir fantasias} {já que} {ninguém fez isso}
+{my boss} {will} {ban costumes} {since} {nobody did it}
 
-{meu chefe} {vai comer salada} {porque eles não podiam}
-{my boss} {will eat salad} {because they couldn't}
+{meu chefe} {vai} {comer salada} {porque} {eles não podiam}
+{my boss} {will} {eat salad} {because} {they couldn't}
 
-{zoe} vai ficar isolada {já que} {ninguém fez isso}
-{zoe} {will be isolated} {since} {nobody did it}
+{zoe} {vai} {ficar isolada} {já que} {ninguém fez isso}
+{zoe} {will} {be isolated} {since} {nobody did it}
 
-{meu chefe} estudava {porque eles não podiam}
-{my boss} {used to study} {because they couldn't}
+{meu chefe} {costumava estudar} {porque} {eles não podiam}
+{my boss} {used to study} {because} {they couldn't}
 
-{zoe} {vai comer saudável} {porque eles não podiam}
-{zoe} {will eat healthy} {because they couldn't}
+{zoe} {vai} {comer saudável} {porque} {eles não podiam}
+{zoe} {will} {eat healthy} {because} {they couldn't}
 
-{joe} {estudou finanças} {porque eles não podiam}
-{joe} {studied finance} {because they couldn't}
+{joe} {estudou finanças} {porque} {eles não podiam}
+{joe} {studied finance} {because} {they couldn't}
 
-{meu chefe} {vai comer salada} {já que} {ninguém fez isso}
-{my boss} {will eat salad} {since} {nobody did it}
+{meu chefe} {vai} {comer salada} {já que} {ninguém fez isso}
+{my boss} {will} {eat salad} {since} {nobody did it}
 `
 export const teach = `
 looks for what the exits are
