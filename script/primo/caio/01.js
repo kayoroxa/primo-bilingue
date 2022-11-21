455a965a3715c48c72bb66610bb6d102b79e58e6
Caio <kayoroxa@gmail.com>
2022-11-20 23:59:02 -0300

    script

--- script/primo/caio/01.js
+++ script/primo/caio/01.js
@@ -1,86 +1,226 @@
 export const rawScript = `
-now
-everyone
-wants to see you
-everyone wants to see you now
-just a few months
-nobody
-nobody study english just a few months
-on the beach
-nobody wants to see you on the beach
-last night
-my cousin
-my cousin study english last night
+para eu me sentir bem
+for me to feel good
+
+elas
+they
+
+sorriu
 smiled
-everyone smiled just a few months
-go buy pizza
-i don't care if
-i don't care if my cousin go buy pizza
+
+eles sorriram para eu me sentir bem
+they smiled for me to feel good        
+
+noite passada
+last night
+
+minha prima
+my cousin
+
+meu primo estudou ingles ontem a noite
+my cousin study english last night     
+
+no meu trabalho
 in my work
-everyone go buy pizza in my work
-good morning
-good morning my cousin study english
-nobody go buy pizza on the beach
-wants to talk to you
-if you want
-nobody wants to talk to you if you want
-everyone smiled on the beach
-since always
-my cousin wants to see you since always
-at the restaurant
-nobody go buy pizza at the restaurant
-everyone wants to see you since always
-i wonder if nobody smiled
-it's been a while
-everyone smiled it's been a while
-everyone go buy pizza on the beach
-everyone go buy pizza since always
-i wonder if my cousin wants to see you
-nobody wants to talk to you now
-nobody smiled last night
-my cousin go buy pizza now
-nobody wants to see you in my work
-nobody smiled it's been a while
-i wonder if my cousin study english
-everyone smiled at the restaurant
-my cousin go buy pizza in my work
-nobody smiled in my work
+
+meu primo sorriu no meu trabalho
 my cousin smiled in my work
-everyone study english in my work
-my cousin wants to see you now
-nobody go buy pizza now
-everyone study english on the beach
-i wonder if everyone go buy pizza
-everyone study english last night
-i don't care if everyone study english
-nobody smiled just a few months
-nobody study english last night
-i wonder if everyone smiled
-nobody go buy pizza if you want
+
+eu me pergunto se meu primo estuda ingles
+i wonder if my cousin study english    
+
+quase todos os dias
+almost every day
+
+pode estar aqui
+can be here
+
+a multidão
+the crowd
+
+a multidão pode estar aqui quase todos os dias
+the crowd can be here almost every day 
+
+apenas alguns meses
+just a few months
+
+eles sorriram apenas alguns meses
+they smiled just a few months
+
+saber como resolver
+to know how to solve
+
+eles estudam inglês para saber como resolver
+they study english to know how to solve
+
+a multidão sorriu ontem à noite
+the crowd smiled last night
+
+na praia
+on the beach
+
+quer falar com você
+wants to talk to you
+
+eles querem falar com você na praia
+they wants to talk to you on the beach 
+
+agora mesmo
+right now
+
+eles querem falar com você agora
+they wants to talk to you right now    
+
+gostaria de te conhecer
+would like to know you
+
+eles gostariam de te conhecer no meu trabalho
+they would like to know you in my work
+
+faz algum tempo
+it's been a while
+
+faz algum tempo
+it's been a while
+
+a multidão pode estar aqui já faz um tempo
+the crowd can be here it's been a while
+
+a galera estudou ingles ontem a noite
+the crowd study english last night
+
+eles podem estar aqui já faz um tempo
+they can be here it's been a while
+
+a multidão sorriu para saber como resolver
+the crowd smiled to know how to solve
+
+eles podem estar aqui na praia
+they can be here on the beach
+
+meu primo estuda ingles no meu trabalho
+my cousin study english in my work
+
+meu primo pode estar aqui quase todos os dias
+my cousin can be here almost every day
+
+eles sorriram na praia
+they smiled on the beach
+
+meu primo sorriu já faz um tempo
+my cousin smiled it's been a while
+
+perto da praia
+near the beach
+
+meu primo sorriu perto da praia
+my cousin smiled near the beach
+
+a multidão sorriu no meu trabalho agora
+the crowd smiled in my work right now
+
+eu não me inclinei porque eles sorriram
+i don't leaned why they smiled
+
+Eu me pergunto se eles sorriram
+i wonder if they smiled
+
+a multidão pode estar aqui perto da praia
+the crowd can be here near the beach
+
+meu primo pode estar aqui perto da praia
+my cousin can be here near the beach
+
+a multidão sorriu já faz um tempo
+the crowd smiled it's been a while
+
+eles podem estar aqui na praia agora
+they can be here on the beach right now
+
+eles estudam inglês apenas alguns meses
+they study english just a few months
+
+meu primo pode estar aqui agora
+my cousin can be here right now
+
+a multidão pode estar aqui agora
+the crowd can be here right now
+
+Eu me pergunto se eles gostariam de te conhecer
+i wonder if they would like to know you
+
+eles querem falar com você no meu trabalho
+they wants to talk to you in my work
+
+eles estudam inglês perto da praia
+they study english near the beach
+
+eu não me inclinei porque eles estudam inglês
+i don't leaned why they study english
+
+porque é bom
+because it's good
+
+meu primo pode estar aqui porque é bom
+my cousin can be here because it's good
+
+Eu me pergunto se a multidão pode estar aqui
+i wonder if the crowd can be here
+
+meu primo sorriu na praia
+my cousin smiled on the beach
+
+a multidão estuda inglês na praia
+the crowd study english on the beach
+
+meu primo sorriu porque é bom
+my cousin smiled because it's good
+
 `
 
 export const teach = `
-everyone
-wants to see you
-now
-nobody
-study English
-just a few months
-tried not to upset you
-at the restaurant
-on the beach
 my cousin
-almost all the time
-last night
 smiled
-go buy pizza
-since always
-i don't care if
-wants to talk to you
-in my work
-good morning
-if you want
+they
+for me to feel good
 I wonder if
-it's been a while
+study English
+last night
+in my work
+the crowd
+can be here
+almost every day
+just a few months
+to know how to solve
+wants to talk to you
 because it's good
+on the beach
+it's been a while
+right now
+would like to know you
+near the beach
+I don't leaned why
+
+
+minha prima
+sorriu
+elas
+para eu me sentir bem
+eu me pergunto se
+estudar inglês
+noite passada
+no meu trabalho
+a multidão
+pode estar aqui
+quase todos os dias
+apenas alguns meses
+saber como resolver
+quer falar com você
+porque é bom
+na praia
+faz algum tempo
+agora mesmo
+gostaria de te conhecer
+perto da praia
+eu não me inclinei porque
 `
